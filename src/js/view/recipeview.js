f230a408b983faaf06e7c3c7486a2faf23fd5e93
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2022-11-07 15:22:21 +0200

    var Fraction = require(fractional).Fractionl

--- src/js/view/recipeview.js
+++ src/js/view/recipeview.js
@@ -1,5 +1,6 @@
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
+// import { Fraction } from 'fractional';
+var Fraction = require('fractional').Fraction;
 import view from './view.js';
 
 class RecipeView extends view {
